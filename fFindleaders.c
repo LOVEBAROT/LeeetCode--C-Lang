8d0b4cf7232e345205dd7681d1956a6225351c0b
LOVE R BAROT <35568343+LOVEBAROT@users.noreply.github.com>
2022-03-14 17:41:12 +0530

    Add files via upload

---
+++ fFindleaders.c
@@ -0,0 +1,19 @@
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        vector<int>v;
+        int max=0;
+       
+        for(int i=n-1;i>=0;i--){
+            if(max<=a[i]){
+                max=a[i];
+                v.push_back(max);
+            }
+        }
+        reverse(v.begin(),v.end());
+        return v;
+        
+    }
+};
